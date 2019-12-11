d0646c73dac667a1519e6a5792692e324f592ac7
caoyao <caoyao@testbird.com>
2019-12-11 18:07:04 +0800

    change head

--- pages/index/index/index.vue
+++ pages/index/index/index.vue
@@ -64,21 +64,6 @@ export default {
             return this.$store.state.lang == 'e_n'
         }
     },
-    head() {
-        return {
-            title: "TestBird - 移动互联测试专家",
-             meta: [
-                {
-                    name: "keyword",
-                    content: "移动广告，TBMedia，TB 网盟，TB Network，海外广告平台，海外移动营销，海外应用推广，海外品牌推广，Facebook广告投放，Google广告投放，海外媒体投放，用户获取，海外广告投放平台、手机游戏测试，手游测试，手游测试工具，手游行业测试解决方案、标准兼容性测试、压力测试，云测试，云测试平台、远程真机调试，APP测试，移动应用测试，应用测试，安卓测试，Android测试，iOS测试，金融APP测试，银行APP测试，证券APP测试，场景测试，金融行业测试解决方案，TROC 移动APP自动化测试平台"
-                },
-                {
-                    name: "description",
-                    content: "TestBird移动互联测试专家，专注为企业提供与移动应用相关的云端测试和移动广告链条式生态服务，目前拥有针对移动广告业务的TB Media、TB 网盟及云测试服务、众测服务、TROC自动化测试平台。为全球移动互联紧密联系的企业提供成熟的行业测试和移动广告解决方案，服务行业涵盖手游、金融、电商、硬件、教育等，业务线包含港澳台、日、韩、欧美等全球市场。依托在北、上、广、深等地分部，持续服务40万开发者，助力企业提升业务效力，在全球市场释放品牌价值。"
-                }
-            ]
-        };
-    },
     components: {
         videoBanner,
         newBox,
